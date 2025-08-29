ea84a952fdf5b9e1e19d58209f8318a889c9d0da
Evans Castro <evanscastro2005@gmail.com>
2025-08-29 08:07:31 -0600

    Primera Subida

---
+++ Paractica_1_Semana_1/HelloMundo/HelloMundo/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace HelloMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.BtIniciar = new System.Windows.Forms.Button();
+            this.ctClave = new System.Windows.Forms.TextBox();
+            this.ctUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.rbFahrenheit = new System.Windows.Forms.RadioButton();
+            this.rbCelsius = new System.Windows.Forms.RadioButton();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtTemperatura = new System.Windows.Forms.TextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.BtIniciar);
+            this.tabPage1.Controls.Add(this.ctClave);
+            this.tabPage1.Controls.Add(this.ctUsuario);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 421);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Login";
+            // 
+            // BtIniciar
+            // 
+            this.BtIniciar.Location = new System.Drawing.Point(383, 114);
+            this.BtIniciar.Name = "BtIniciar";
+            this.BtIniciar.Size = new System.Drawing.Size(232, 64);
+            this.BtIniciar.TabIndex = 9;
+            this.BtIniciar.Text = "Iniciar Sesion";
+            this.BtIniciar.UseVisualStyleBackColor = true;
+            this.BtIniciar.Click += new System.EventHandler(this.BtIniciar_Click_1);
+            // 
+            // ctClave
+            // 
+            this.ctClave.Location = new System.Drawing.Point(139, 159);
+            this.ctClave.Name = "ctClave";
+            this.ctClave.PasswordChar = '*';
+            this.ctClave.Size = new System.Drawing.Size(163, 22);
+            this.ctClave.TabIndex = 8;
+            // 
+            // ctUsuario
+            // 
+            this.ctUsuario.Location = new System.Drawing.Point(139, 108);
+            this.ctUsuario.Name = "ctUsuario";
+            this.ctUsuario.Size = new System.Drawing.Size(163, 22);
+            this.ctUsuario.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Contraseña:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Usuario:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtAltura);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.txtPeso);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.resultado);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 421);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "IMC";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(181, 138);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 22);
+            this.txtAltura.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese Altura:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(176, 61);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(105, 22);
+            this.txtPeso.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ingrese Peso: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(348, 90);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(181, 40);
+            this.resultado.TabIndex = 0;
+            this.resultado.Text = "Calcular";
+            this.resultado.UseVisualStyleBackColor = true;
+            this.resultado.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.rbFahrenheit);
+            this.tabPage3.Controls.Add(this.rbCelsius);
+            this.tabPage3.Controls.Add(this.btnConvertir);
+            this.tabPage3.Controls.Add(this.lblResultado);
+            this.tabPage3.Controls.Add(this.txtTemperatura);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(792, 421);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Conversor";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // rbFahrenheit
+            // 
+            this.rbFahrenheit.AutoSize = true;
+            this.rbFahrenheit.Location = new System.Drawing.Point(350, 105);
+            this.rbFahrenheit.Name = "rbFahrenheit";
+            this.rbFahrenheit.Size = new System.Drawing.Size(91, 20);
+            this.rbFahrenheit.TabIndex = 4;
+            this.rbFahrenheit.TabStop = true;
+            this.rbFahrenheit.Text = "Fahrenheit";
+            this.rbFahrenheit.UseVisualStyleBackColor = true;
+            // 
+            // rbCelsius
+            // 
+            this.rbCelsius.AutoSize = true;
+            this.rbCelsius.Location = new System.Drawing.Point(222, 105);
+            this.rbCelsius.Name = "rbCelsius";
+            this.rbCelsius.Size = new System.Drawing.Size(72, 20);
+            this.rbCelsius.TabIndex = 3;
+            this.rbCelsius.TabStop = true;
+            this.rbCelsius.Text = "Celsius";
+            this.rbCelsius.UseVisualStyleBackColor = true;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(428, 152);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(143, 47);
+            this.btnConvertir.TabIndex = 2;
+            this.btnConvertir.Text = "Conversion";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(121, 167);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(99, 16);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "Ingrese el valor";
+            this.lblResultado.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtTemperatura
+            // 
+            this.txtTemperatura.Location = new System.Drawing.Point(226, 167);
+            this.txtTemperatura.Name = "txtTemperatura";
+            this.txtTemperatura.Size = new System.Drawing.Size(133, 22);
+            this.txtTemperatura.TabIndex = 0;
+            this.txtTemperatura.TextChanged += new System.EventHandler(this.txtTemperatura_TextChanged);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.lblContador);
+            this.tabPage4.Controls.Add(this.btnContar);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(792, 421);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Contador de Clics";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(358, 62);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(170, 64);
+            this.btnContar.TabIndex = 0;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(244, 86);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(62, 16);
+            this.lblContador.TabIndex = 1;
+            this.lblContador.Text = "Contador";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Mi primera aplicacion";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button BtIniciar;
+        private System.Windows.Forms.TextBox ctClave;
+        private System.Windows.Forms.TextBox ctUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button resultado;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtTemperatura;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.RadioButton rbFahrenheit;
+        private System.Windows.Forms.RadioButton rbCelsius;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.Button btnContar;
+    }
+}
+
---
+++ Paractica_1_Semana_1/HelloMundo/HelloMundo/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BtIniciar.Click += BtIniciar_Click;  // <-- Esto conecta el botón con el evento
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int intentos = 0;
+        private void BtIniciar_Click(object sender, EventArgs e)
+        {
+            string usuario = ctUsuario.Text;
+            string clave = ctClave.Text;
+
+            if (usuario == "Evans" && clave == "230423")
+            {
+                MessageBox.Show("Bienvenido al Sistema " + usuario);
+                Close();
+            }
+            else
+            {
+               intentos++;
+                if (intentos >= 3)
+                {
+                    MessageBox.Show("Has excedido el número de intentos. El programa se cerrará.");
+                    Application.Exit();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o Clave incorrecta. Intento " + intentos + " de 3.");
+                    ctUsuario.Focus();
+                    ctClave.Clear();
+                }
+            }
+            
+        }
+
+        private void ctUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double peso, altura;
+
+            if (!double.TryParse(txtPeso.Text, out peso) || !double.TryParse(txtAltura.Text, out altura))
+            {
+                MessageBox.Show("Ingrese valores numericos validos.");
+                return;
+            }
+
+            if (altura <= 0)
+            {
+                MessageBox.Show("La Altura debe ser mayor a 0.");
+                return;
+            }
+
+            double imc = peso / (altura * altura);
+            string categoria = "";
+
+            if (imc < 18.5)
+                categoria = "Bajo peso";
+            else if (imc < 24.9)
+                categoria = "Normal";
+            else if (imc < 29.9)
+                categoria = "Sobrepeso";
+            else if (imc < 34.9)
+                categoria = "Obesidad Clase 1";
+            else if (imc < 39.9)
+                categoria = "Obesidad Clase 2";
+            else
+                categoria = "Obesidad Clase 3";
+
+            MessageBox.Show($"IMC: {imc:F2} - {categoria}");
+        }
+
+        private void BtIniciar_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            double valor;
+
+            if (!double.TryParse(txtTemperatura.Text, out valor))
+            {
+                MessageBox.Show("Ingrese un número válido.");
+                return;
+            }
+
+            if (rbCelsius.Checked) // Convertir de Celsius a Fahrenheit
+            {
+                double fahrenheit = (valor * 9 / 5) + 32;
+                lblResultado.Text = valor + " °C = " + fahrenheit.ToString("F2") + " °F";
+            }
+            else if (rbFahrenheit.Checked) // Convertir de Fahrenheit a Celsius
+            {
+                double celsius = (valor - 32) * 5 / 9;
+                lblResultado.Text = valor + " °F = " + celsius.ToString("F2") + " °C";
+            }
+
+        }
+
+        private void txtTemperatura_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        int contador = 0;
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            contador++;
+            lblContador.Text = "Clics: " + contador;
+
+        }
+    }
+}
